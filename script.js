8dc1032ea22bb147b9f96c3cc6d160bfa54a9058
prdz14 <121666972+prdz14@users.noreply.github.com>
2023-06-15 15:39:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-// const data = new Promise((resolve, reject) => {
-//     let user = {
-//         name: 'Alex',
-//         age: 20
-//     }
-//     setTimeout(() => {
-//         console.log("Загрузка данных с сервера...")
-//         resolve(user)
-//     }, 2000);
-// }).then(data => {
-//     console.log(data)
-// })
+
 
 const video = document.querySelector('video')
 
@@ -21,12 +10,3 @@ navigator.mediaDevices.getUserMedia({
 }).catch((error) => {
     console.log('Ошибка подключения', error);
 })
-
-// const btn = document.querySelector('button')
-
-// btn.addEventListener('click', () => getInfo())
-
-// function getInfo() {
-//     fetch('https://jsonplaceholder.typicode.com/users')
-//         .then(data => console.log(data))
-// }
